69a65532ad8fae6909115886ca3789ddafdc2af1
Mellang <dlwpgh23@naver.com>
2022-01-14 17:53:16 +0900

    add pakage

--- src/components/feed/GroupFeed.tsx
+++ src/components/feed/GroupFeed.tsx
@@ -1,11 +1,61 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Loading from "../Loading";
 
 export default function GroupFeed() {
   const [posts, setPosts] = useState<Object[]>([{}]);
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function readPost() {
+      try {
+        const {
+          data: { data },
+        } = await axios.post(`http://localhost:5000/post/read`, {
+          group_id: "61cf23fade9e5f953c747b90",
+          page: 1,
+        });
+        console.log(data);
+        setPosts(data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    readPost();
+    console.log(loading, posts);
+  }, []);
+
   if (loading) {
     return <Loading />;
   }
-  return <div>그룹피드</div>;
+  return (
+    <div>
+      <h1>그룹피드</h1>
+      <div>
+        {posts.map((item: any) => {
+          return (
+            <div
+              style={{ display: "flex", flexDirection: "column" }}
+              key={item._id}
+            >
+              <div>
+                <div>{item.group_name}</div>
+                <div>{item.text}</div>
+                <div>{item.created_at}</div>
+              </div>
+              <div>
+                {item.comment.map((commentItme: any) => {
+                  return (
+                    <div style={{ backgroundColor: "beige" }}>
+                      {commentItme.text}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function ProfileHeader(){
-    return <div>프로파일 헤더</div>
+export default function ProfileHeader() {
+  return (
+    <div>
+      <h1>프로파일 헤더</h1>
+      <div>
+        <div>내정보</div>
+        <div>친구</div>
+        <div>팔로우</div>
+      </div>
+    </div>
+  );
 }
