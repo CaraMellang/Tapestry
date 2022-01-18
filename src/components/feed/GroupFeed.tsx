cbd384d144b83b4d34f12e9c8701cbbbdd80f1c9
Mellang <dlwpgh23@naver.com>
2022-01-18 22:21:52 +0900

    부분적 해결, 무분별한 api 요청 문제

--- src/components/feed/GroupFeed.tsx
+++ src/components/feed/GroupFeed.tsx
@@ -4,65 +4,94 @@ import { useInView } from "react-intersection-observer";
 import Loading from "../Loading";
 
 export default function GroupFeed() {
-  const [posts, setPosts] = useState<object[]>([
-    { group_name: "ㅗ" },
-    { group_name: "ㅗ" },
-    { group_name: "ㅗ" },
-    { group_name: "ㅗ" },
-    { group_name: "ㅗ" },
-    { group_name: "ㅗ" },
-  ]);
-  let items = [];
-  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState<object[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  let isLoading = false;
+  // const [isInterSecting, setIsInterSecting] = useState<boolean>(false)
   // const [observerRef, inView] = useInView();
   const [page, setPage] = useState(1);
   const observerTarget = useRef(null);
-  const [target, setTarget] = useState();
+  const [target, setTarget] = useState<HTMLElement | null | undefined>(null);
 
-  
+  // // 서버에서 아이템을 가지고 오는 함수
+  // const getItems = useCallback(async () => {
+  //   setLoading(true);
 
-  const onIntersect = useCallback(async (
-    entries: IntersectionObserverEntry[],
-    observer: IntersectionObserver
-  ) => {
-    console.log("옵저버 실행?");
-    console.log(entries);
-    console.log(observer);
-    if (entries[0].intersectionRatio <= 0) return;
-    if (entries[0].isIntersecting && loading === false) {
-      // observer.unobserve(entries[0].target)
-      setLoading(true);
-      try {
-        const {
-          data: { data },
-        } = await axios.post(`http://localhost:5000/post/read`, {
-          group_id: "61cf23fade9e5f953c747b90",
-          page: page,
-        });
-        setPosts((prevState) => [...prevState, ...data]);
-        setPage((prevState) => prevState + 1);
+  //   const {
+  //     data: { data },
+  //   } = await axios.post(`http://localhost:5000/post/read`, {
+  //     group_id: "61cf23fade9e5f953c747b90",
+  //     page: page,
+  //   });
+  //   console.log(data);
+  //   setPosts((prevState) => [...prevState, ...data]);
+  //   setLoading(false);
+  // }, [page]);
+
+  // // `getItems` 가 바뀔 때 마다 함수 실행
+  // useEffect(() => {
+  //   getItems();
+  // }, [getItems]);
+
+  // useEffect(() => {
+  //   // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
+  //   if (inView && !loading) {
+  //     setPage((prevState) => prevState + 1);
+  //   }
+  // }, [inView, loading]);
+
+  const onIntersect = useCallback(
+    async (
+      entries: IntersectionObserverEntry[],
+      observer: IntersectionObserver
+    ) => {
+      // console.log("옵저버 실행?");
+      // console.log(entries);
+      // console.log(observer);
+      console.log(entries[0].intersectionRatio);
+      console.log("isinter", entries[0].isIntersecting);
+      // console.log("page", page);
+      console.log("loading", loading);
+      if (entries[0].intersectionRatio <= 0)
+        return console.log("인터섹션라티오", entries[0].intersectionRatio);
+      if (entries[0].isIntersecting && loading === false) {
+        // observer.unobserve(entries[0].target)
+        try {
+          // setLoading(true);
+          isLoading  = true
+          const {
+            data: { data },
+          } = await axios.post(`http://localhost:5000/post/read`, {
+            group_id: "61cf23fade9e5f953c747b90",
+            page: page,
+          });
+          setPosts((prevState) => [...prevState, ...data]);
+          setPage((prevState) => prevState + 1);
+          console.log(data);
+          console.log(page);
+          console.log(loading);
+          console.log(loading);
+        } catch (err) {
+          console.log(err);
+        }
         // observer.observe(entries[0].target)
-        console.log(data);
-        console.log(page);
-        console.log(loading)
-        setLoading(false);
-        console.log(loading)
-      } catch (err) {
-        // observer.observe(entries[0].target)
-        setLoading(false);
-        console.log(err);
+        // setLoading(false);
+        isLoading  = false
       }
-    }
-  },[page])
+    },
+    [page]
+  );
 
   useEffect(() => {
-    const intersectionObserver = new IntersectionObserver(onIntersect, {
-      threshold: 0.4,
-    });
-    if (observerTarget.current) {
-      intersectionObserver.observe(observerTarget.current);
+    if (target) {
+      console.log(page);
+      let intersectionObserver = new IntersectionObserver(onIntersect, {
+        threshold: 0.4,
+      });
+      intersectionObserver.observe(target);
+      return () => intersectionObserver && intersectionObserver.disconnect();
     }
-  }, []);
+  }, [target, page]);
 
   return (
     <div>
@@ -96,7 +125,7 @@ export default function GroupFeed() {
           );
         })}
         {loading ? <Loading /> : ""}
-        <div ref={observerTarget}></div>
+        <div ref={setTarget}>{loading.toString()}</div>
       </div>
     </div>
   );
