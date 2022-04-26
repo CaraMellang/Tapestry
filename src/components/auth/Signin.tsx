39d886adc62cfda533485161e54e8a76a92f7b72
Mellang <dlwpgh23@naver.com>
2022-04-27 00:29:00 +0900

    remove antd design in signIn component

--- src/components/auth/Signin.tsx
+++ src/components/auth/Signin.tsx
@@ -1,6 +1,6 @@
 import { Button, Form, Input } from "antd";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import useInput from "../../hook/useInput";
@@ -16,6 +16,7 @@ interface SignInProps {
 export default function Signin({ setIsSign, setSigninToggle }: SignInProps) {
   const [email, setEmail] = useInput("");
   const [password, setPassword] = useInput("");
+  const [emailError, setEmailError] = useState<null | string>(null);
   const dispatch = useDispatch();
 
   const userSelector: any = useSelector((state: any) => state.userSliceReducer);
@@ -25,8 +26,15 @@ export default function Signin({ setIsSign, setSigninToggle }: SignInProps) {
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     // e.preventDefault(); antd는 안써도됨
+    e.preventDefault();
     if (email === "" || password === "") {
       window.alert("require Email && Password!");
+      return;
+    }
+    let reg =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    if (!reg.test(email)) {
+      setEmailError("잘못된 형식의 이메일입니다.");
       return;
     }
 
@@ -47,7 +55,10 @@ export default function Signin({ setIsSign, setSigninToggle }: SignInProps) {
 
   return (
     <SignInWrap>
-      <Form onFinish={onSubmit} style={{ color: "inherit" }}>
+      <div style={{ marginBottom: "32px" }}>
+        <div className="signInHead">SignIn</div>
+      </div>
+      {/* <Form onFinish={onSubmit} className="signInFormTag">
         <Form.Item
           // label="Email"
           name="email"
@@ -59,6 +70,7 @@ export default function Signin({ setIsSign, setSigninToggle }: SignInProps) {
             value={email}
             onChange={setEmail}
             placeholder="Email"
+            className="signInFormTagInput"
           />
         </Form.Item>
         <Form.Item
@@ -71,6 +83,7 @@ export default function Signin({ setIsSign, setSigninToggle }: SignInProps) {
             value={password}
             onChange={setPassword}
             placeholder="Password"
+            className="signInFormTagInput"
           />
         </Form.Item>
         <a href={`${httpPath}/auth/google`} className="theme-bg-element2">
@@ -90,18 +103,74 @@ export default function Signin({ setIsSign, setSigninToggle }: SignInProps) {
         >
           아직 회원이 아니신가요?
         </div>
-      </Form>
+      </Form> */}
+      <form onSubmit={onSubmit}>
+        <div style={{ marginBottom: "28px" }}>
+          <input
+            type={"email"}
+            className="signInFormTagInput"
+            value={email}
+            onChange={setEmail}
+            placeholder="Email"
+          />
+          <div className="inputWarning">{emailError}</div>
+        </div>
+        <div style={{ marginBottom: "28px" }}>
+          <input
+            type={"password"}
+            className="signInFormTagInput"
+            value={password}
+            onChange={setPassword}
+            placeholder="Password"
+          />
+          <div className="inputWarning"></div>
+        </div>
+        <div style={{ marginBottom: "28px" }}>
+          <button type="submit">로그인</button>
+        </div>
+      </form>
+      <div className="theme-bg-element2" onClick={() => setSigninToggle(false)}>
+        아직 회원이 아니신가요?
+      </div>
     </SignInWrap>
   );
 }
 
 const SignInWrap = styled.div`
-  /* justify-content: center; */
-  /* width: 200px; */
-  /* margin: auto; */
-  /* box-sizing: content-box; */
-  /* padding: 20px; */
-  /* border: #9b9b9b solid 1px; */
+  display: flex;
+  flex-direction: column;
   border-radius: 12px;
   padding: 4rem;
+  .signInHead {
+    width: fit-content;
+    font-size: 24px;
+    font-weight: bold;
+    color: var(--primary1);
+    border-bottom: 5px solid var(--primary1);
+    padding-bottom: 2px;
+    margin: auto;
+  }
+  .signInFormTag {
+    color: inherit;
+  }
+  .signInFormTagInput {
+    width: 100%;
+    background-color: var(--bg-element2);
+    color: var(--color-text);
+    border: 0;
+    padding-bottom: 0.5rem;
+    border-bottom: 2px solid var(--primary1);
+  }
+  .inputWarning {
+    margin: 8px 0;
+    color: #c21842;
+    height: 25px;
+  }
+  button {
+    width: 100%;
+    background: var(--primary1);
+    padding: 12px 0;
+    border-radius: 4px;
+    cursor: pointer;
+  }
 `;
