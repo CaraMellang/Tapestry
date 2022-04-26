8055d7d5d87ddc57b98f3675a62949a3886ce41b
Mellang <dlwpgh23@naver.com>
2022-04-26 17:02:03 +0900

    design banner?

--- src/components/auth/SignBanner.tsx
+++ src/components/auth/SignBanner.tsx
@@ -2,7 +2,54 @@ import React from "react";
 import styled from "styled-components";
 
 export default function SignBanner() {
-  return <SignBannerWrap>배너(사라지면 모바일 모드임)</SignBannerWrap>;
+  return (
+    <SignBannerWrap>
+      <div>welcome to tapestry</div>
+      <div className="circle circle1 theme-bg-element2">
+        <div className="innetCircle" />
+      </div>
+      {/* <div className="circle circle2" />
+      <div className="circle circle3" /> */}
+    </SignBannerWrap>
+  );
 }
 
-const SignBannerWrap = styled.div``;
+const SignBannerWrap = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: #3aad6d;
+  border-radius: 1rem 0 0 1rem;
+  overflow: hidden;
+  .circle {
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    /* background: #63e6be; */
+    /* border: 2rem solid #63e6be; */
+    border-radius: 100px;
+  }
+  .circle1 {
+    top: 50%;
+    right: -100px;
+  }
+  .innetCircle {
+    position: absolute;
+    background: #3aad6d;
+    width: 126px;
+    height: 126px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 63px;
+  }
+  .circle2 {
+    bottom: -120px;
+    left: calc(50% + 20px);
+    transform: translate(-50%);
+  }
+  .circle3 {
+    bottom: -50px;
+    right: -50px;
+  }
+`;
