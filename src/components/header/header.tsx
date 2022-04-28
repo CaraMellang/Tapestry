1c6bcc3462afa6f12240ac052cf765047c07591d
Mellang <dlwpgh23@naver.com>
2022-04-28 16:37:09 +0900

    remove antd tag && styling header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,11 +5,13 @@ import { useSelector } from "react-redux";
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { deleteCookie } from "../../lib/cookie";
+import Search from "../common/Search";
+import { ReactComponent as FeedIcon } from "../static/svg/feedIcon.svg";
 
 function Header() {
   const navigate = useNavigate();
   const selector = useSelector((state: any) => state.userSliceReducer);
-  const { Search } = Input;
+  // const { Search } = Input;
 
   console.log();
   const onSearch = (e: string) => {
@@ -19,43 +21,74 @@ function Header() {
 
   return (
     <HeaderWrap>
-      <Search placeholder="input search text" onSearch={onSearch} enterButton />
-      <NavLink
-        to={`/`}
-        style={({ isActive }) => ({
-          color: isActive ? `green` : `inherit`,
-        })}
-      >
-        메인~
-      </NavLink>
-      <NavLink
-        to={`/feed`}
-        style={({ isActive }) => ({
-          color: isActive ? `green` : `inherit`,
-        })}
-      >
-        피드
-      </NavLink>
-      <NavLink
-        to={`/profile/${selector.user.userId}`}
-        style={({ isActive }) => ({
-          color: isActive ? `green` : `inherit`,
-        })}
-      >
-        프로파일
-      </NavLink>
-      <div
-        className="theme-bg-element2"
-        onClick={() => {
-          deleteCookie("access_token");
-          window.location.replace(`/`);
-        }}
-      >
-        로그아웃
+      <div>
+        <Link to={`/`}>Tapestry</Link>
+      </div>
+      {/* <div style={{ width: "33.3%" }}>Tapestry</div> */}
+      {/* <Search placeholder="input search text" onSearch={onSearch} enterButton /> */}
+      <div style={{ display: "flex" }}>
+        {/* <NavLink
+          to={`/`}
+          style={({ isActive }) => ({
+            color: isActive ? `green` : `inherit`,
+          })}
+        >
+          메인~
+        </NavLink>
+        <NavLink
+          to={`/feed`}
+          style={({ isActive }) => ({
+            color: isActive ? `green` : `inherit`,
+          })}
+        >
+          피드
+        </NavLink> */}
+        <Link to={`/feed`}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <FeedIcon
+              style={{
+                width: "30px",
+                height: "30px",
+              }}
+            />
+            <div>새글피드</div>
+          </div>
+        </Link>
+        <NavLink
+          to={`/profile/${selector.user.userId}`}
+          style={({ isActive }) => ({
+            color: isActive ? `green` : `inherit`,
+          })}
+        >
+          프로파일
+        </NavLink>
+        <Search />
+        <div
+          className="theme-bg-element2"
+          onClick={() => {
+            deleteCookie("access_token");
+            window.location.replace(`/`);
+          }}
+        >
+          로그아웃
+        </div>
       </div>
     </HeaderWrap>
   );
 }
 export default Header;
 
-const HeaderWrap = styled.div``;
+const HeaderWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 1340px;
+  margin: auto;
+  @media (max-width: 1440px) {
+    width: 924px;
+  }
+`;
