cb107e071559eb43f52ad638f278319db83fdc8d
Mellang <dlwpgh23@naver.com>
2022-05-01 15:43:22 +0900

    add response header

---
+++ src/components/header/DesktopHeader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function DesktopHeader() {
+  return <DesktopHeaderWrap>데스크톱</DesktopHeaderWrap>;
+}
+
+const DesktopHeaderWrap = styled.header``;
---
+++ src/components/header/MobileHeader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function MobileHeader(){
+    return <MobileHeaderWrap>모바일</MobileHeaderWrap>
+}
+
+const MobileHeaderWrap = styled.header``
