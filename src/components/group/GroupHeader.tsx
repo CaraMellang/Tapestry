b387c0876eba9109150d17e4359d0fc76eb41fa5
Mellang <dlwpgh23@naver.com>
2022-05-04 00:06:24 +0900

    그룹 헤더 css 변경

--- src/components/group/GroupHeader.tsx
+++ src/components/group/GroupHeader.tsx
@@ -5,28 +5,37 @@ import styled from "styled-components";
 export default function GroupHeader() {
   const { _id } = useParams();
   return (
-    <GroupHeaderWrap>
-      <div>그룹에어리어헤더</div>
-      <div style={{ textAlign: "center" }}>
-        <NavLink
-          to={`/group/${_id}/post`}
-          style={({ isActive }) => ({
-            color: isActive ? `green` : `inherit`,
-          })}
-        >
-          게시글
-        </NavLink>
-        <NavLink
-          to={`/group/${_id}/member`}
-          style={({ isActive }) => ({
-            color: isActive ? `green` : `inherit`,
-          })}
-        >
-          멤버
-        </NavLink>
-      </div>
+    <GroupHeaderWrap className="theme-bg-element2">
+      <NavLink
+        to={`/group/${_id}/post`}
+        style={({ isActive }) => ({
+          color: isActive ? `green` : `inherit`,
+          borderBottom: `2px solid ${isActive ? "green" : "var(--color-text)"}`,
+        })}
+      >
+        게시글
+      </NavLink>
+      <NavLink
+        to={`/group/${_id}/member`}
+        style={({ isActive }) => ({
+          color: isActive ? `green` : `inherit`,
+          borderBottom: `2px solid ${isActive ? "green" : "var(--color-text)"}`,
+        })}
+      >
+        멤버
+      </NavLink>
     </GroupHeaderWrap>
   );
 }
 
-const GroupHeaderWrap = styled.div``;
+const GroupHeaderWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
+  margin: 1rem 0;
+  padding: 1rem 0.5rem;
+  border-radius: 0.5rem;
+  a {
+    padding-bottom: 1px;
+  }
+`;
