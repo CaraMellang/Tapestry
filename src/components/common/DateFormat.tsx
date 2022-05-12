de1ea1134f74a2bd90ee203e6f88f9bc6825abd8
Mellang <dlwpgh23@naver.com>
2022-05-12 15:26:26 +0900

    DateFormat Hook

--- src/components/common/DateFormat.tsx
+++ src/components/common/DateFormat.tsx
@@ -1,27 +1,40 @@
 import dayjs from "dayjs";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 interface DateFormatProps {
   dateValue: string | Date;
 }
 
-export default function DateFormat({ dateValue }: DateFormatProps) {
+export default function DateFormat(dateValue: string | Date) {
   const [format, setFormat] = useState(
     dayjs(dateValue).format("YYYY년MM월DD일 HH:mm")
   );
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     let currentDate = new Date();
     let itemDate = dayjs(dateValue);
     if (
       currentDate.getFullYear() === itemDate.get("year") &&
-      currentDate.getMonth() + 1 === itemDate.get("month") &&
+      currentDate.getMonth() + 1 === itemDate.get("month") + 1 &&
+      currentDate.getDate() === itemDate.get("date") &&
+      currentDate.getHours() - dayjs(itemDate).get("hour") === 0
+    ) {
+      setFormat(
+        `${currentDate.getMinutes() - dayjs(itemDate).get("minute")}분 전`
+      );
+    } else if (
+      currentDate.getFullYear() === itemDate.get("year") &&
+      currentDate.getMonth() + 1 === itemDate.get("month") + 1 &&
       currentDate.getDate() === itemDate.get("date")
     ) {
-      setFormat(dayjs(itemDate).format("HH시간 전"));
+      setFormat(
+        `${currentDate.getHours() - dayjs(itemDate).get("hour")}시간 전`
+      );
+    } else if (itemDate.get("hour") < 12) {
+      setFormat(dayjs(itemDate).format(`YYYY년MM월DD일 오전 HH:mm`));
+    } else if (itemDate.get("hour") >= 12) {
+      setFormat(dayjs(itemDate).format(`YYYY년MM월DD일 오후 hh:mm`));
     }
-    if (itemDate.get("month") >= 12) {
-    }
-  }, []);
-  return format
+  }, [dateValue]);
+  return format;
 }
