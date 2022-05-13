fb9b80144e2ffb392eb6536b5621b48a5504c3df
Mellang <dlwpgh23@naver.com>
2022-05-13 16:20:28 +0900

    리덕스이용 => 로컬 컴포넌트로 변경

--- src/components/feed/GroupFeed.tsx
+++ src/components/feed/GroupFeed.tsx
@@ -2,65 +2,67 @@ import axios from "axios";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 import {
   GROUP_FEED_EMPTY,
   GROUP_FEED_REQUEST,
 } from "../../modules/redux/GroupFeed";
+import PostLayout from "../common/postItem/PostLayout";
 import Loading from "../Loading";
 
 export default function GroupFeed() {
-  const [target, setTarget] = useState<HTMLElement | null | undefined>(null);
-  const groupFeedSelector = useSelector(
-    (state: any) => state.groupFeedSliceReducer
-  );
-  const userSelector = useSelector((state: any) => state.userSliceReducer);
-  const groupsSelector = useSelector((state: any) => state.groupsSliceReducer);
-  const dispatch = useDispatch();
+  // const [target, setTarget] = useState<HTMLElement | null | undefined>(null);
+  // const groupFeedSelector = useSelector(
+  //   (state: any) => state.groupFeedSliceReducer
+  // );
+  // const userSelector = useSelector((state: any) => state.userSliceReducer);
+  // const groupsSelector = useSelector((state: any) => state.groupsSliceReducer);
+  // const dispatch = useDispatch();
 
-  const onIntersect = useCallback(
-    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-      if (entries[0].intersectionRatio <= 0)
-        return console.log("인터섹션라티오", entries[0].intersectionRatio);
-      if (
-        entries[0].isIntersecting &&
-        groupFeedSelector.groupFeedLoading === false
-      ) {
-        console.log(groupsSelector.groups);
-        const data = {
-          group_arr: groupsSelector.groups,
-          page: groupFeedSelector.groupPageNumber,
-        };
-        dispatch(GROUP_FEED_REQUEST(data));
-      }
-    },
-    [
-      groupFeedSelector.groupPageNumber,
-      groupFeedSelector.groupFeedLoading,
-      groupsSelector.group,
-    ]
-  );
+  // const onIntersect = useCallback(
+  //   (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+  //     if (entries[0].intersectionRatio <= 0)
+  //       return console.log("인터섹션라티오", entries[0].intersectionRatio);
+  //     if (
+  //       entries[0].isIntersecting &&
+  //       groupFeedSelector.groupFeedLoading === false
+  //     ) {
+  //       console.log(groupsSelector.groups);
+  //       const data = {
+  //         group_arr: groupsSelector.groups,
+  //         page: groupFeedSelector.groupPageNumber,
+  //       };
+  //       dispatch(GROUP_FEED_REQUEST(data));
+  //     }
+  //   },
+  //   [
+  //     groupFeedSelector.groupPageNumber,
+  //     groupFeedSelector.groupFeedLoading,
+  //     groupsSelector.group,
+  //   ]
+  // );
 
-  useEffect(() => {
-    if (target) {
-      let intersectionObserver = new IntersectionObserver(onIntersect, {
-        threshold: 0.4,
-      });
-      intersectionObserver.observe(target);
-      return () => {
-        intersectionObserver && intersectionObserver.disconnect();
-      };
-    }
-  }, [target, onIntersect]);
+  // useEffect(() => {
+  //   if (target) {
+  //     let intersectionObserver = new IntersectionObserver(onIntersect, {
+  //       threshold: 0.4,
+  //     });
+  //     intersectionObserver.observe(target);
+  //     return () => {
+  //       intersectionObserver && intersectionObserver.disconnect();
+  //     };
+  //   }
+  // }, [target, onIntersect]);
 
-  useEffect(() => {
-    return () => {
-      dispatch(GROUP_FEED_EMPTY("dummy"));
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     dispatch(GROUP_FEED_EMPTY("dummy"));
+  //   };
+  // }, []);
 
   return (
-    <div>
-      <h1 className="dd">그룹피드{groupFeedSelector.groupPageNumber}</h1>
+    <GroupFeedWrap>
+      {/* <h1 className="dd">그룹피드{groupFeedSelector.groupPageNumber}</h1>
       <div>
         {groupFeedSelector.groupFeeds.map((item: any, index: number) => {
           return (
@@ -102,7 +104,10 @@ export default function GroupFeed() {
           <div>더이상 게시글이 없습니다.</div>
         )}
         <div ref={setTarget}>인식범위 입니다.(옵저버)</div>
-      </div>
-    </div>
+      </div> */}
+      <PostLayout />
+    </GroupFeedWrap>
   );
 }
+
+const GroupFeedWrap = styled.div``
