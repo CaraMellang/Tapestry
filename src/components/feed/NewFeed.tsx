e8243457c04fddf7691ee4ece0e2e741ee32d1b1
Mellang <dlwpgh23@naver.com>
2022-05-16 22:21:29 +0900

    주석 제거

--- src/components/feed/NewFeed.tsx
+++ src/components/feed/NewFeed.tsx
@@ -5,11 +5,6 @@ import PostLayout from "../common/postItem/PostLayout";
 import Loading from "../Loading";
 
 export default function NewFeed() {
-  // const [loading, setLoading] = useState(true);
-
-  // if (loading) {
-  //   return <Loading />;
-  // }
   return (
     <NewFeedWrap>
       <PostLayout />
