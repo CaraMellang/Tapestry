35442cc7096b33ca16b4723ebefc183d908e2d9b
Mellang <dlwpgh23@naver.com>
2022-05-20 17:56:56 +0900

    사용증명

--- src/modules/saga/UserSaga.tsx
+++ src/modules/saga/UserSaga.tsx
@@ -28,7 +28,7 @@ export function* postUser(action: any): Generator {
 }
 
 function postToken(data: any) {
-  return client.post(`/auth/verify`, ".");
+  return client.post(`/auth/verify`, ".", { withCredentials: true });
 }
 
 export function* postUserToken(action: any): Generator {
